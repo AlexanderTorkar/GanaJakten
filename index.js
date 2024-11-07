db49d2d65800fc70b0c7f8816df01a2c5da177e5
Ad4msvensson <adampereric@icloud.com>
2024-11-07 08:57:32 +0100

    res.sendfile change

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/html/index.html'));
+    res.sendFile(path.join(__dirname, 'public/css/main.css'));
+
 });
 
 app.listen(1235);
