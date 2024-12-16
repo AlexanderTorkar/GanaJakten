9e5a20ce89fa62a25503de9c6670a60f970adaa9
Ad4msvensson <adampereric@icloud.com>
2024-12-16 09:57:39 +0100

    jka

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/html/index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 
 });
 
