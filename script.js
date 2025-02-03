d4e4162e847715dcabf58ee669cff532f4e31993
Gustafpieper <113342781+Gustafpieper@users.noreply.github.com>
2025-02-03 10:09:56 +0100

    counter

--- script.js
+++ script.js
@@ -88,6 +88,12 @@ function checkAnswer(selectedIndex) {
         localStorage.setItem("score", score);
     }
 
+    let count = parseInt(document.getElementById("display").textContent);
+    if (count < 10) {
+        count++;
+        document.getElementById("display").textContent = count;
+    }
+
     // Move to the next question
     currentQuestionIndex++;
     localStorage.setItem("answeredQuestions", currentQuestionIndex);
@@ -135,6 +141,9 @@ window.onpopstate = () => {
     window.location.href = "index.html";
 };
 
+if (performance.getEntriesByType("navigation")[0].type === "reload") {
+    window.location.href = "index.html"; // Redirect to start page
+}
 
 
 if ('serviceWorker' in navigator) {
